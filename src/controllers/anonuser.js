fd704c20f8d3022f4f5a3dfbeeaad2cb751c83ca
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-07-05 09:29:13 +0300

    return the hash

--- src/controllers/anonuser.js
+++ src/controllers/anonuser.js
@@ -1,14 +1,16 @@
 const uuidv4 = require('uuid/v4')
+const crypto = require('crypto')
 
 module.exports = (app, db) => {
   app.get('/users', (req, res) =>
     db.models.AnonUser.findAll().then((result) => res.json(result))
   )
   app.post("/user", (req, res) => {
+    const hash = crypto.createHash('md5').update("" + (Math.random() * 99999999) + Date.now()).digest("hex")
     db.models.AnonUser.create({
       id: uuidv4(),
-      entry_hash: 'test'
+      entry_hash: hash
     })
-    res.json({status: 'ok'})
+    res.json({status: 'ok', hash: hash})
   })
 }
