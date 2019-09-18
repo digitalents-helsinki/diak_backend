7bf6a9002d2a2a25ae87cfcfafb015b2c65f27cb
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-09-18 14:25:56 +0300

    wip

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ config.config()
 const db = require('./models')
 const apiUser = require('./controllers/anonuser')
 const apiResult = require('./controllers/result')
+const apiLogin = require('./controllers/login')
 // const routes = require('./router')
 
 
@@ -17,6 +18,7 @@ app.use(cors())
 
 apiUser(app, db)
 apiResult(app, db)
+apiLogin(app)
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
