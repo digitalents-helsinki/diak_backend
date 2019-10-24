f3343c9d24df0287a8c16a2c0c25394573995544
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2019-10-24 16:13:59 +0300

    Add endpoint for sending survey result to user email

--- src/controllers/result.js
+++ src/controllers/result.js
@@ -1,4 +1,5 @@
 const uuidv4 = require('uuid/v4')
+const sendMail = require('../mail')
 const checkToken = require('../jwt')
 
 module.exports = (app, db) => {
@@ -59,6 +60,9 @@ module.exports = (app, db) => {
       transaction = await db.sequelize.transaction();
 
       const anonuser = await db.AnonUser.findOne({
+        where: {
+          entry_hash: req.body.anonId
+        },
         attributes: ["id"],
         lock: true,
         rejectOnEmpty: true,
@@ -100,6 +104,78 @@ module.exports = (app, db) => {
       else res.json({status: "not ok"})
     }
 
+  })
+  app.post("/emailresult", async (req, res) => {
+
+    const AnonUser = await db.AnonUser.findOne({
+      where: {
+        entry_hash: req.body.anonId
+      },
+      attributes: ["id"]
+    })
+
+    const QuestionsAnswers = await db.Question.findAll({
+      where: {
+        SurveySurveyId: req.body.surveyId
+      },
+      include: {
+        model: db.Answer,
+        where: {
+          AnonUserId: AnonUser.id
+        }
+      }
+    })
+
+    const defaultTitles = {
+      health: 'Terveys',
+      overcoming: 'Resilienssi',
+      living: 'Asuminen',
+      coping: 'Pärjääminen',
+      family: 'Perhesuhteet',
+      friends: 'Ystävyyssuhteet',
+      finance: 'Talous',
+      strengths: 'Itsensä kehittäminen',
+      self_esteem: 'Itsetunto',
+      life_as_whole: 'Elämään tyytyväisyys'
+    }
+
+    const tableContents = QuestionsAnswers.sort((a, b) => a.number - b.number).reduce((contents, obj, idx) => {
+      return `
+        ${contents}
+        <tr>
+          <td style="border: 1px solid grey; border-left: none;${idx === 0 ? ' border-top: 1px solid black;' : idx + 1 === QuestionsAnswers.length ? ' border-bottom: none;' : ''} text-align: center;">
+            ${defaultTitles[obj.name] || obj.title}
+          </td>
+          <td style="border: 1px solid grey;${idx === 0 ? ' border-top: 1px solid black;' : idx + 1 === QuestionsAnswers.length ? ' border-bottom: none;' : ''} text-align: center;">
+            ${obj.Answers[0].value === null ? '' : obj.Answers[0].value}
+          </td>
+          <td style="border: 1px solid grey; border-right: none;${idx === 0 ? ' border-top: 1px solid black;' : idx + 1 === QuestionsAnswers.length ? ' border-bottom: none;' : ''} text-align: center;">
+            ${obj.Answers[0].description || ''}
+          </td>
+        </tr>
+        `
+    }, '')
+
+    sendMail(req.body.email, 'Vastauksesi kyselyyn', 
+      `Tässä ovat vastauksesi täyttämääsi kyselyyn:
+      <br><br>
+      <table style="border: 2px solid black; border-collapse: separate; border-spacing: 0; width: 100%;">
+        <tr>
+          <td style="border: 1px solid black; border-left: none; border-top: none; text-align: center; font-size: 18px; font-weight: bold;">
+            Kysymys
+          </td>
+          <td style="border: 1px solid black; border-top: none; text-align: center; font-size: 18px; font-weight: bold;">
+            Arvo
+          </td>
+          <td style="border: 1px solid black; border-right: none; border-top: none; text-align: center; font-size: 18px; font-weight: bold;">
+            Kuvaus
+          </td>
+        </tr>
+        ${tableContents}
+      </table>
+      `)
+    
+    res.send("Email sent")
   })
   app.post("/testresult", async (req, res) => {
     let testikysely = await db.Survey.findOne({
