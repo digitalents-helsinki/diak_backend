65849af1b84417506df03fff26e39f4590347e14
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2019-12-11 13:48:34 +0200

    Improve error error

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,6 +1,6 @@
 const { StatusError, AuthError } = require('../customErrors')
 const { JsonWebTokenError } = require('jsonwebtoken')
-const { EmptyResultError, ValidationError } = require('sequelize')
+const { EmptyResultError, ValidationError, UniqueConstraintError } = require('sequelize')
 
 module.exports = (app) => {
   app.use((err, req, res, next) => {
@@ -8,26 +8,26 @@ module.exports = (app) => {
       return next(err)
     }
     console.error(err.stack)
-    if (err instanceof StatusError) {
-      return res.status(err.status).send(err.message)
+
+    switch (err.constructor) {
+      case StatusError:
+        return res.status(err.status).send(err.message)
+      case AuthError:
+        if (err.authenticated) {
+          return res.status(403).send(err.message)
+        } else {
+          return res.append('WWW-Authenticate', 'Bearer').status(401).send(err.message)
+        }
+      case JsonWebTokenError:
+        return res.status(401).send(err.message)
+      case EmptyResultError:
+        return res.sendStatus(404)
+      case UniqueConstraintError:
+        return res.status(409).json(Object.values(err.fields))
+      case ValidationError:
+        return res.status(422).json(`${err.errors.map(error => error.value)}`)
+      default:
+        return res.sendStatus(500)
     }
-    if (err instanceof AuthError) {
-      if (err.authenticated) {
-        return res.status(403).send(err.message)
-      } else {
-        return res.append('WWW-Authenticate', 'Bearer').status(401).send(err.message)
-      }
-    }
-    if (err instanceof JsonWebTokenError) {
-      return res.status(401).send(err.message)
-    }
-    if (err instanceof EmptyResultError) {
-      return res.sendStatus(404)
-    }
-    if (err instanceof ValidationError) {
-      return res.status(422).send("Validation failed")
-    }
-    
-    return res.sendStatus(500)
   })
 }
