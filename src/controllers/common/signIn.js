5a432143b1f6e5355ee734c7db2e5dacdf78eee5
Zohaib Malik <zohaibmalik100.zm@gmail.com>
2020-01-08 13:53:45 +0200

    admin name and email in admin side

--- src/controllers/common/signIn.js
+++ src/controllers/common/signIn.js
@@ -63,7 +63,17 @@ module.exports = wrapAsync(async (req, res, next) => {
     return res.cookie('Ctx', ctx, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production'
-    }).json({ userId: userRecord.userId, token: token, role: userRecord.role })
+    }).json({
+      userId: userRecord.userId, 
+      token: token, 
+      role: userRecord.role,
+      name: userRecord.name,
+      email:userRecord.email,
+      post_number:userRecord.post_number,
+      phone_number:userRecord.phone_number,
+      age:userRecord.age,
+      gender:userRecord.gender
+    })
   } else {
     await Promise.all([limiterSlowBruteByIp.penalty(ipAddr), limiterConsecutiveFailsByEmailAndIp.penalty(emailIpKey)]) // apply both penalties
     return res.sendStatus(401)
