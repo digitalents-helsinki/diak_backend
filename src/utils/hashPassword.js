a9a6aa10d6ee3fc17cb86da836a61920abbb8272
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-01-16 14:26:54 +0200

    Adjust argon2 parameters such that they work with one cpu core

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -3,6 +3,6 @@ const argon2 = require('argon2')
 module.exports = async (password) => await argon2.hash(password, {
   timeCost: 10,
   memoryCost: 256000,
-  parallelism: 8,
+  parallelism: 2,
   type: argon2.argon2id
 })
