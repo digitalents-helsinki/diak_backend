3b7c77c3aa4e78f67e8f7dc5e01b66585b234f79
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-01-16 15:42:51 +0200

    Fix path

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ const umzug = new Umzug({
     sequelize: db.sequelize
   },
   migrations: {
-    path: process.cwd() + '/src/migrations'
+    path: './src/migrations'
   }
 })
 // If you need to change the database, write a migration file in the migrations folder and it will be automatically executed
