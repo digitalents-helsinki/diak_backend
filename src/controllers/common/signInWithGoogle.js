2cb85d059e619cb7cccb2c4b67ed8ec12c0fed67
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-01-24 10:46:15 +0200

    Remove useless env vars and add params to migrations

--- src/controllers/common/signInWithGoogle.js
+++ src/controllers/common/signInWithGoogle.js
@@ -7,7 +7,7 @@ const hashPassword = require('../../utils/hashPassword')
 const { OAuth2Client } = require('google-auth-library')
 
 module.exports = wrapAsync(async (req, res, next) => {
-  const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'postmessage')
+  const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
   const ticket = await client.verifyIdToken({
     idToken: req.body.id_token,
     audience: process.env.GOOGLE_CLIENT_ID
