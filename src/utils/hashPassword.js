a869acb0345484b73ca22035d7845d1023362f3c
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-01-27 16:42:27 +0200

    Remove useless async await

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -1,6 +1,6 @@
 const argon2 = require('argon2')
 
-module.exports = async (password) => await argon2.hash(password, {
+module.exports = (password) => argon2.hash(password, {
   timeCost: 5,
   memoryCost: 128000,
   parallelism: 2,
